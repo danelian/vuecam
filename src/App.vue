9364c873bc53131f09376c5eb665a829a25e71cc
danelian <141317269+danelian@users.noreply.github.com>
2023-11-03 07:28:47 +0300

    all functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-
+import Camera from './components/Camera.vue'
 </script>
 
 <template>
-
+  <div class="min-h-screen flex items-center justify-center">
+    <Camera />
+  </div>
 </template>
-
-<style scoped>
-
-</style>
